d4b2b619805b62766af5bff75367e214e395488d
MGCPC <812457592@qq.com>
2026-07-03 19:19:40 +0800

    修改了一些bug，还有很多bug

--- studyplatform-vue/src/layouts/AppShell.vue
+++ studyplatform-vue/src/layouts/AppShell.vue
@@ -68,12 +68,15 @@ const shellClass = computed(() => ({
   'app-page well-log-shell': route.path === '/lab/well-log',
   'app-page visual-shell': route.path.startsWith('/visualization'),
   'app-page games-shell': route.path.startsWith('/games'),
+  'immersive-game-shell': route.path === '/games/ladder-jump',
 }))
+
+const showNavigation = computed(() => route.path !== '/games/ladder-jump')
 </script>
 
 <template>
   <div :class="shellClass">
-    <AppNavigation :nav-items="navItems" />
+    <AppNavigation v-if="showNavigation" :nav-items="navItems" />
     <RouterView />
   </div>
 </template>
